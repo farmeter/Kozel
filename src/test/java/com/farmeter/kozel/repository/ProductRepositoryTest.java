53abe828b8f63fa43e133718dff3780a87f4c46d
farmeter <farmeter@gmail.com>
2020-02-28 18:00:39 +0900

    add base test

--- src/test/java/com/farmeter/kozel/repository/ProductRepositoryTest.java
+++ src/test/java/com/farmeter/kozel/repository/ProductRepositoryTest.java
@@ -1,20 +1,15 @@
-package com.farmeter.kozel;
+package com.farmeter.kozel.repository;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.farmeter.kozel.AbstractBaseTest;
 import com.farmeter.kozel.config.SampleData;
 import com.farmeter.kozel.entity.Product;
-import com.farmeter.kozel.repository.ProductRepository;
 import com.farmeter.kozel.util.CommonUtils;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-public class ProductRepositoryTest {
+public class ProductRepositoryTest extends AbstractBaseTest {
 
     @Autowired
     private ProductRepository productRepository;
