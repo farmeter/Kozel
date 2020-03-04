c927d236391725528d5e37a39875c0893d80edb5
farmeter <farmeter@gmail.com>
2020-03-04 17:50:25 +0900

    remove unused line

--- src/main/java/com/farmeter/kozel/entity/Product.java
+++ src/main/java/com/farmeter/kozel/entity/Product.java
@@ -1,18 +1,13 @@
 package com.farmeter.kozel.entity;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
