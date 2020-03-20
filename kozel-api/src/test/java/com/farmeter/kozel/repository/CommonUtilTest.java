f75a270db7df0d2df0cb9ff7124dace9e5a109d1
farmeter <farmeter@gmail.com>
2020-03-20 12:09:07 +0900

    add testcase log

--- kozel-api/src/test/java/com/farmeter/kozel/repository/CommonUtilTest.java
+++ kozel-api/src/test/java/com/farmeter/kozel/repository/CommonUtilTest.java
@@ -2,19 +2,19 @@ package com.farmeter.kozel.repository;
 
 import org.junit.Test;
 
-//import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 
 import com.farmeter.kozel.AbstractBaseTest;
 import com.farmeter.kozel.entity.Product;
 import com.farmeter.kozel.util.CommonUtils;
 
-//@Slf4j
-public class CommonUtilTest /**extends AbstractBaseTest**/ {
+@Slf4j
+public class CommonUtilTest extends AbstractBaseTest {
 
     @Test
     public void test() {
-//        Product sample = Product.builder().name("productName1").originPrice(100L).salePrice(50L).build();
-//        log.info("{}" , sample);
-//        log.info("{}" , CommonUtils.toJson(sample));
+        Product sample = Product.builder().name("productName1").originPrice(100L).salePrice(50L).build();
+        log.info("{}" , sample);
+        log.info("{}" , CommonUtils.toJson(sample));
     }
 }
