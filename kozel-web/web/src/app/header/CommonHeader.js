4814d1d22a4483c9012627b62d674efe0a80f686
farmeter <farmeter@gmail.com>
2020-05-06 18:28:27 +0900

    remove logo

--- kozel-web/web/src/app/header/CommonHeader.js
+++ kozel-web/web/src/app/header/CommonHeader.js
@@ -4,9 +4,6 @@ import '../style/CommonHeader.css';
 const CommonHeader = ({form, children}) => {
     return (
         <main className="header-list-template">
-            <div className="logo">
-                <img width="100" src="https://www.pngitem.com/pimgs/m/485-4852378_sample-logo-png-transparent-png.png" />
-            </div>
             <div className="title">
                 Header
             </div>
