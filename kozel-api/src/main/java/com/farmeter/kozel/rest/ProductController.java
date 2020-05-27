4021220717783c56b9949f1074811c434d90b906
farmeter <farmeter@gmail.com>
2020-05-27 19:13:28 +0900

    add api save, delete operations

--- kozel-api/src/main/java/com/farmeter/kozel/rest/ProductController.java
+++ kozel-api/src/main/java/com/farmeter/kozel/rest/ProductController.java
@@ -3,8 +3,11 @@ package com.farmeter.kozel.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.farmeter.kozel.entity.Product;
@@ -25,4 +28,15 @@ public class ProductController {
     public List<Product> products() {
         return productRepository.findAll();
     }
+
+    @PostMapping("/product")
+    public void productSave(Product product) {
+        productRepository.save(product);
+    }
+
+    @DeleteMapping("/product")
+    public void productDelete(Product product) {
+        productRepository.delete(product);
+    }
+
 }
