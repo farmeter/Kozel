6056768246631368cfacb7ae8348258b773fa379
farmeter <farmeter@gmail.com>
2020-10-23 19:36:35 +0900

    add react simple web

--- kozel-web/src/js/components/Header/index.js
+++ kozel-web/src/js/components/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <h1 className="title">Simple React Template</h1>
+                <h1 className="title">Simple React Header</h1>
             </header>
         )
     }
