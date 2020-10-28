92f1aa50b29c5ecd4c36c2273e5d28ab840586a0
farmeter <farmeter@gmail.com>
2020-10-28 20:26:15 +0900

    add api module

--- kozel-web/src/js/components/Header/index.js
+++ kozel-web/src/js/components/Header/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import { getGoogle } from '../../service/serviceExmaple'
 
 class Header extends Component {
 
     render() {
+
+        const result = getGoogle();
+        console.log("===HELLO Header===");
+        console.log(result);
+
         return (
             <header>
                 <h1 className="title">Simple React Header</h1>
--- kozel-web/src/js/service/serviceExmaple.js
+++ kozel-web/src/js/service/serviceExmaple.js
@@ -1,9 +1,14 @@
 const request = require('request');
 
-request('http://www.google.com', function (error, response, body) {
-    //callback
-});
+const getGoogle = () => {
+    request('http://www.google.com', function (error, response, body) {
+        return response;
+    });
+}
+
 
 request.get({uri:"http://www.google.com"}, function (error, response, body) {
     //callback
 });
+
+export { getGoogle };
